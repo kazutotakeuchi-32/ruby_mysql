0dd7209d7eca5c7cbfc02486d273319bab18e281
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2022-01-31 22:27:46 +0900

    api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+require "/Users/takeuchikazuto/project/ruby_mysql/config/controller.rb"
+require "/Users/takeuchikazuto/project/ruby_mysql/app/model/user.rb"
+
+class Api::V1::UsersController  < Controller
+  def self.index
+  end
+  def self.show(params=nil)
+  end
+  def self.create(params=nil)
+  end
+  def self.update(params=nil)
+  end
+end
