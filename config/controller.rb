40b9222c0b25ed0c374af69497182716bdf3b21a
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2022-02-06 21:11:30 +0900

    Controller#render 拡張

--- config/controller.rb
+++ config/controller.rb
@@ -2,8 +2,17 @@ require "erb"
 require "json/add/core"
 
 class Controller
-  def self.render(erb,response_type=nil)
-    ERB.new(erb).result(binding)
+  def self.render(params,response_type=nil)
+    case response_type
+    when :xml
+      # xml
+    when :json
+      self.render_json(params)
+    when :html 
+      ERB.new(erb).result(binding)
+    else
+      ERB.new(erb).result(binding)
+    end
   end
   def self.render_json(params)
     params.to_json
